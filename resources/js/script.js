1618aff1b1064af1c4af44951e673ca577fd3671
adrian-rutkowski <60190502+adrian-rutkowski@users.noreply.github.com>
2021-03-09 12:04:50 +0100

    Project finished.

---
+++ resources/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plan').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+       $('.js--wp-1').addClass('aniamted fadeIn'); 
+    });
+    
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round')
+        }
+    });
+    
+});
